5ecc09ea027c6f9369ea10e995d7408bce1479ad
nipunthathsara <nipunthathsara@gmail.com>
2020-08-19 01:22:00 +0530

    Create osgi bundle

---
+++ src/main/java/org/wso2/carbon/custom/userstore/manager/CustomUserStoreManager.java
@@ -0,0 +1,11 @@
+package org.wso2.carbon.custom.userstore.manager;
+
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.ldap.UniqueIDReadWriteLDAPUserStoreManager;
+
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+
+public class CustomUserStoreManager extends UniqueIDReadWriteLDAPUserStoreManager {
+
+}
